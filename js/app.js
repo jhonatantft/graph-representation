30854865540f29c57945a3ba82fd5abff661e7a2
jhonatantft <jhonatan.tft@gmail.com>
2020-04-17 18:14:47 -0300

    ajusts

--- js/app.js
+++ js/app.js
@@ -54,198 +54,195 @@ document.addEventListener('DOMContentLoaded', function () {
     var windowHeight = window.innerHeight - 100;
     init(windowWidth, windowHeight);
 
-    document.getElementById("create-graph").onclick = function() {
-        //parse data
-        var gdataformatvalue = gdataformatselect.value;
-        var gdatatextareavalue = gdatatextarea.value;
-        var gtype = document.querySelector('input[name = "graph-type"]:checked').value;
-        var etype = document.querySelector('input[name = "edge-type"]:checked').value;
-
-        var dataset = {
-            nodes: [],
-            edges: []
-        };
-
-        if (gtype == "directed") {
-            directed = true;
-        } else {
-            directed = false;
-        }
-
-        if (etype == "weighted") {
-            weighted = true;
-        } else {
-            weighted = false;
-        }
-
-        if (labelformatselect.value == "custom") {
-            custom_labels = true;
-        } else {
-            custom_labels = false;
-        }
-
-        var node_labels_value = nodelabelstextarea.value.trim();
-        var node_list = [];
-        if (node_labels_value != "" && node_labels_value != undefined)
-            node_list = node_labels_value.split('\n')
-
-        // console.log(node_list);
-        var node_dict = {};
-        var edges_dict = {};
-        for (var i = 0; i < node_list.length; i++) {
-            var node_name = node_list[i].trim();
-            if (!(node_name in node_dict)) {
-                node_dict[node_name] = i;
-                edges_dict[i] = {};
-                dataset.nodes.push({
-                    name: node_name
-                });
-            } else {
-                alert("Multiple nodes with same labels! Please remove them");
-                return false;
-            }
-        }
-        // console.log(node_dict);
-
-        if (gdatatextareavalue != "" && gdatatextareavalue != undefined) {
-            var data_list = gdatatextareavalue.split('\n');
-
-            if (gdataformatvalue == "abw") {
-                for (var i = 0; i < data_list.length; i++) {
-                    abw = data_list[i].trim().split(/\s+/);
-                    // console.log(abw);
-                    if (abw.length < 2) {
-                        alert('Invalid edge format ' + data_list[i]);
-                        return false;
-                    }
-                    if (!(abw[0] in node_dict) || !(abw[1] in node_dict)) {
-                        alert("One of the nodes not in nodes list " + abw[0] + " or " + abw[1]);
-                        return false;
-                    }
-                    var a = node_dict[abw[0]],
-                        b = node_dict[abw[1]];
-                    var label = "";
-                    if (abw.length > 2) {
-                        label = abw[2].trim();
-                    }
-                    if (!directed && (a in edges_dict[b])) {
-                        edges_dict[b][a] += ", " + label;
-                    } else {
-                        if (!(b in edges_dict[a])) {
-                            edges_dict[a][b] = label;
-                        } else {
-                            edges_dict[a][b] += ", " + label;
-                        }
-                    }
-                }
-                for (var i in edges_dict) {
-                    for (var j in edges_dict[i]) {
-                        var edge_type_value = NORMAL_EDGE;
-                        if (i == j) edge_type_value = SELF_EDGE;
-                        else if (directed && edges_dict[j][i]) edge_type_value = DIRECTED_BIEDGE;
-                        dataset.edges.push({
-                            source: i,
-                            target: j,
-                            label: edges_dict[i][j],
-                            edge_type: edge_type_value
-                        });
-                    }
-                }
-            } else {
-                var rl = data_list.length;
-                if (rl != dataset.nodes.length) {
-                    alert("Not a square matrix or some nodes missing");
+    /**
+     * Creates graph based on user inputs
+     */
+    function createsGraph () {
+        document.getElementById("create-graph").onclick = function () {
+            //parse data
+            var gdataformatvalue = gdataformatselect.value;
+            var gdatatextareavalue = gdatatextarea.value;
+            var gtype = document.querySelector('input[name = "graph-type"]:checked').value;
+            var etype = document.querySelector('input[name = "edge-type"]:checked').value;
+    
+            var dataset = {
+                nodes: [],
+                edges: []
+            };
+    
+            directed = Boolean(gtype === 'directed');
+            weighted = Boolean(etype === 'weighted');
+            custom_labels = Boolean(labelformatselect.value == 'custom');
+    
+            var node_labels_value = nodelabelstextarea.value.trim();
+            var node_list = [];
+            if (node_labels_value != "" && node_labels_value != undefined)
+                node_list = node_labels_value.split('\n')
+    
+            // console.log(node_list);
+            var node_dict = {};
+            var edges_dict = {};
+            for (var i = 0; i < node_list.length; i++) {
+                var node_name = node_list[i].trim();
+                if (!(node_name in node_dict)) {
+                    node_dict[node_name] = i;
+                    edges_dict[i] = {};
+                    dataset.nodes.push({
+                        name: node_name
+                    });
+                } else {
+                    alert("Vários vértices com o mesmo rótulo, remova!");
                     return false;
                 }
-                var edge_matrix = [];
-                for (var i = 0; i < data_list.length; i++) {
-                    var vals = data_list[i].split(',');
-                    if (rl != vals.length) {
-                        alert("Not a square matrix  or some nodes missing");
-                        return false;
-                    }
-                    edge_matrix.push(vals);
-                }
-                for (var i = 0; i < edge_matrix.length; i++) {
-                    for (var j = 0; j < edge_matrix[i].length; j++) {
-                        if (edge_matrix[i][j].trim() == "") {
-                            alert("Use '0' for denoting no edge");
+            }
+            // console.log(node_dict);
+    
+            if (gdatatextareavalue != "" && gdatatextareavalue != undefined) {
+                var data_list = gdatatextareavalue.split('\n');
+    
+                if (gdataformatvalue == "abw") {
+                    for (var i = 0; i < data_list.length; i++) {
+                        abw = data_list[i].trim().split(/\s+/);
+                        // console.log(abw);
+                        if (abw.length < 2) {
+                            alert('Aresta inválida ' + data_list[i]);
                             return false;
                         }
-                        if (edge_matrix[i][j].trim() != "0") {
+                        if (!(abw[0] in node_dict) || !(abw[1] in node_dict)) {
+                            alert("Um dos vértices não está na lista de vértices " + abw[0] + " ou " + abw[1]);
+                            return false;
+                        }
+                        var a = node_dict[abw[0]],
+                            b = node_dict[abw[1]];
+                        var label = "";
+                        if (abw.length > 2) {
+                            label = abw[2].trim();
+                        }
+                        if (!directed && (a in edges_dict[b])) {
+                            edges_dict[b][a] += ", " + label;
+                        } else {
+                            if (!(b in edges_dict[a])) {
+                                edges_dict[a][b] = label;
+                            } else {
+                                edges_dict[a][b] += ", " + label;
+                            }
+                        }
+                    }
+                    for (var i in edges_dict) {
+                        for (var j in edges_dict[i]) {
                             var edge_type_value = NORMAL_EDGE;
                             if (i == j) edge_type_value = SELF_EDGE;
-                            else if (directed && edge_matrix[j][i].trim() != "0") edge_type_value = DIRECTED_BIEDGE;
+                            else if (directed && edges_dict[j][i]) edge_type_value = DIRECTED_BIEDGE;
                             dataset.edges.push({
                                 source: i,
                                 target: j,
-                                label: edge_matrix[i][j].trim(),
+                                label: edges_dict[i][j],
                                 edge_type: edge_type_value
                             });
                         }
                     }
+                } else {
+                    var rl = data_list.length;
+                    if (rl != dataset.nodes.length) {
+                        alert("Not a square matrix or some nodes missing");
+                        return false;
+                    }
+                    var edge_matrix = [];
+                    for (var i = 0; i < data_list.length; i++) {
+                        var vals = data_list[i].split(',');
+                        if (rl != vals.length) {
+                            alert("Not a square matrix  or some nodes missing");
+                            return false;
+                        }
+                        edge_matrix.push(vals);
+                    }
+                    for (var i = 0; i < edge_matrix.length; i++) {
+                        for (var j = 0; j < edge_matrix[i].length; j++) {
+                            if (edge_matrix[i][j].trim() == "") {
+                                alert("Use '0' for denoting no edge");
+                                return false;
+                            }
+                            if (edge_matrix[i][j].trim() != "0") {
+                                var edge_type_value = NORMAL_EDGE;
+                                if (i == j) edge_type_value = SELF_EDGE;
+                                else if (directed && edge_matrix[j][i].trim() != "0") edge_type_value = DIRECTED_BIEDGE;
+                                dataset.edges.push({
+                                    source: i,
+                                    target: j,
+                                    label: edge_matrix[i][j].trim(),
+                                    edge_type: edge_type_value
+                                });
+                            }
+                        }
+                    }
                 }
             }
-        }
-
-        // console.log(dataset);
-        // return false;
-
-        init(parseInt(window.getComputedStyle(document.getElementById("create-graph")).width.split("px")[0]), window.innerHeight - 100);
-        if (dataset.nodes.length > 0) {
-
-            var radius = 12;
-            var linkDistance = 200;
-            var repulsion_force = -1000;
-            var repulsion_force_max_distance = 500;
-            var gravity = 0.01;
-            var link_strength = 0.09;
-
-            if (dataset.nodes.length >= 70) {
-                radius = 4;
-                repulsion_force = -400;
-                repulsion_force_max_distance = 300;
-
-            } else if (dataset.nodes.length >= 50) {
-                radius = 6;
-                repulsion_force = -600;
-                repulsion_force_max_distance = 400;
-
-            } else if (dataset.nodes.length >= 30) {
-                repulsion_force = -800;
-                repulsion_force_max_distance = 500;
-                radius = 8;
+    
+            // console.log(dataset);
+            // return false;
+    
+            init(parseInt(window.getComputedStyle(document.getElementById("create-graph")).width.split("px")[0]), window.innerHeight - 100);
+            if (dataset.nodes.length > 0) {
+    
+                var radius = 12;
+                var linkDistance = 200;
+                var repulsion_force = -1000;
+                var repulsion_force_max_distance = 500;
+                var gravity = 0.01;
+                var link_strength = 0.09;
+    
+                if (dataset.nodes.length >= 70) {
+                    radius = 4;
+                    repulsion_force = -400;
+                    repulsion_force_max_distance = 300;
+    
+                } else if (dataset.nodes.length >= 50) {
+                    radius = 6;
+                    repulsion_force = -600;
+                    repulsion_force_max_distance = 400;
+    
+                } else if (dataset.nodes.length >= 30) {
+                    repulsion_force = -800;
+                    repulsion_force_max_distance = 500;
+                    radius = 8;
+                }
+                create_graph(dataset, linkDistance, repulsion_force, repulsion_force_max_distance, gravity, link_strength, radius, directed, weighted, custom_labels);
             }
-            create_graph(dataset, linkDistance, repulsion_force, repulsion_force_max_distance, gravity, link_strength, radius, directed, weighted, custom_labels);
         }
     }
 
-    //save to png
-    document.getElementById("save").onclick = function() {
-        var svg = d3.select('#svg-space');
-        var canvas = d3.select('canvas').attr('width', svg.attr("width")).attr("height", svg.attr("height")).node();
-        var img = d3.select('body').append('img').attr('width', svg.attr("width")).attr('height', svg.attr("height")).style("display", "none").node();
-        var doctype = '<?xml version="1.0" standalone="no"?>' + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
-        // serialize our SVG XML to a string.
-        var source = (new XMLSerializer()).serializeToString(svg.node());
-        var blob = new Blob([doctype + source], {
-            type: 'image/svg+xml;charset=utf-8'
-        });
-        var url = window.URL.createObjectURL(blob);
-        img.src = url;
-        img.onload = function() {
-            var context = canvas.getContext('2d');
-            context.font = "12px Raleway";
-            context.drawImage(img, 0, 0);
-            var a = document.createElement("a");
-            a.download = "graph_save.png";
-            a.href = canvas.toDataURL("image/png");
-            a.click();
-            a.remove();
-        };
-        img.remove();
+    /**
+     * Saves created graph as a png image
+     */
+    function saveGraphToPng () {
+        document.getElementById('save').onclick = function () {
+            var svg = d3.select('#svg-space');
+            var canvas = d3.select('canvas').attr('width', svg.attr("width")).attr("height", svg.attr("height")).node();
+            var img = d3.select('body').append('img').attr('width', svg.attr("width")).attr('height', svg.attr("height")).style("display", "none").node();
+            var doctype = '<?xml version="1.0" standalone="no"?>' + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+            // serialize our SVG XML to a string.
+            var source = (new XMLSerializer()).serializeToString(svg.node());
+            var blob = new Blob([doctype + source], {
+                type: 'image/svg+xml;charset=utf-8'
+            });
+            var url = window.URL.createObjectURL(blob);
+            img.src = url;
+            img.onload = function() {
+                var context = canvas.getContext('2d');
+                context.font = "12px Raleway";
+                context.drawImage(img, 0, 0);
+                var a = document.createElement("a");
+                a.download = "graph_save.png";
+                a.href = canvas.toDataURL("image/png");
+                a.click();
+                a.remove();
+            };
+            img.remove();
+        }
     }
 
+    createsGraph();
+    saveGraphToPng();
 }, false);
 
 function init (w, h) {
@@ -498,7 +495,7 @@ function create_graph(dataset, linkDistance, repulsion_force, repulsion_force_ma
 }
 
 function change_radius(radius) {
-    d3.selectAll(".nodelabel").attr("dx", radius + 5);
+    d3.selectAll(".nodelabel").attr("dx", radius + 55);
     d3.selectAll("circle").attr("r", radius);
     d3.selectAll(".markers").attr("refX", radius + 15);
 }
